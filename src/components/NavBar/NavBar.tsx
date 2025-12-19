907a41bae6cc21a00c25c760b75cbc2d540744f3
Koteykee <koteykeethecat@gmail.com>
2025-12-19 13:37:20 +0300

    NavBar done

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,81 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserStore } from "../../stores/userStore";
+import { Search } from "../Search/Search";
 
-// import styles from "./NavBar.module.css";
+import styles from "./NavBar.module.css";
+import menuIcon from "../../assets/icons/menu.svg";
 
 export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const { currentUser, loadFromStorage, logoutUser } = useUserStore();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const goMainPage = () => {
+    navigate("/");
+  };
+
+  const handleLogout = () => {
+    logoutUser();
+    toggleMenu();
+    goMainPage();
+  };
+
   return (
-    <nav>
+    <nav className={styles["nav-bar"]}>
       <Link to="/">
-        <h1>Superhero Wiki</h1>
+        <h2 className={styles["title"]}>Superheroes</h2>
       </Link>
+      <Search />
+      {!currentUser && (
+        <Link to="/signin" className={styles["sign-in"]}>
+          <h2 className={styles["title"]}>Sign In</h2>
+        </Link>
+      )}
+      {currentUser && (
+        <div className={styles["menu-wrapper-container"]}>
+          <div className={styles["menu-wrapper"]}>
+            <img
+              src={menuIcon}
+              alt="Menu"
+              className={styles["menu-icon"]}
+              onClick={toggleMenu}
+            />
+            <h3 className={styles["currentUser"]} onClick={toggleMenu}>
+              {currentUser.username}
+            </h3>
+          </div>
+
+          {isMenuOpen && (
+            <ul className={styles["menu-container"]}>
+              <li className={styles["menu-item"]}>
+                <Link
+                  to={`/mysuperheroes`}
+                  className={styles["menu-inner"]}
+                  onClick={toggleMenu}
+                >
+                  My superheroes
+                </Link>
+              </li>
+              <li className={styles["menu-item"]}>
+                <p onClick={handleLogout} className={styles["menu-inner"]}>
+                  Log Out
+                </p>
+              </li>
+            </ul>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
